bb49f1a071d15caf8dad49eb90d89d965e47d8b5
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-05-03 18:44:49 -0400

    feat: Update security configs

--- src/main/java/io/cinema/mstheaterseatmanagement/MsTheaterSeatManagementApplication.java
+++ src/main/java/io/cinema/mstheaterseatmanagement/MsTheaterSeatManagementApplication.java
@@ -4,9 +4,13 @@ import io.cinema.config.AuditingConfig;
 import io.cinema.controller.ExceptionHandlers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 
 @SpringBootApplication
+@EnableReactiveMethodSecurity
+@ComponentScan(basePackages = "io.cinema")
 @Import({ExceptionHandlers.class, AuditingConfig.class})
 public class MsTheaterSeatManagementApplication {
 
